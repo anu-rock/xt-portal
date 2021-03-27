b3b54e3999b1513d24cccb6ac4cd5e444dd01f7a
Anurag Bhandari <anurag.bhd@gmail.com>
2021-03-27 19:26:41 +0530

    Remove broken link - docs/

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ export default function Home() {
           <div className={styles.buttons}>
             <Link
               className={clsx('button button--outline button--secondary button--lg', styles.getStarted)}
-              to={useBaseUrl('docs/')}
+              to={useBaseUrl('learn/')}
             >
               Get Started
             </Link>
